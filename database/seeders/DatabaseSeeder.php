2cd06e4abcb728a1fe691231e1b78f1b01a9b494
Quyen <lethibichquyen5804@gmail.com>
2025-01-25 12:20:13 +0700

    set up source code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            RolesSeeder::class,
-            UsersSeeder::class,
-        ]);
+        // $this->call([
+        //     RolesSeeder::class,
+        //     UsersSeeder::class,
+        // ]);
 
     }
 }
