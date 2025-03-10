7698520e45044e0bb45791f371ccbda88c8c7466
ThuyNgan015 <145478844+ThuyNgan015@users.noreply.github.com>
2025-03-10 13:58:35 +0700

    Feature/search api for order (#38)
    * install redis
    
    * fix api checkout auth for product
    
    * update order status
    
    * fix api search order
    
    * fix api update status order

--- routes/api.php
+++ routes/api.php
@@ -79,6 +79,12 @@ Route::group(['prefix' => 'stores/{storeId}/products', 'middleware' => 'auth:api
 
 Route::group(['prefix' => 'stores/{storeId}/orders', 'middleware' => 'auth:api'], function () {
     Route::get('/', [OrderController::class, 'getOrdersByStore']);
+    Route::get('/deleted', [OrderController::class, 'getDeletedOrders']);
+    Route::get('/{orderId}', [OrderController::class, 'getOrderDetail']);
+    Route::put('/{orderId}/status', [OrderController::class, 'updateOrderStatus']);
+    Route::delete('/{orderId}', [OrderController::class, 'deleteOrder']);
+    Route::delete('/{orderId}/force', [OrderController::class, 'forceDeleteOrder']);
+    Route::post('/{orderId}/restore', [OrderController::class, 'restoreOrder']);
 });
 
 Route::post('/upload-image', [ImageController::class, 'upload']);
